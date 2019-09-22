211c02204ef93d91c66c68e4d7b540d82ff8c88d
Dan Gold <tentativechaos@gmail.com>
2019-09-21 18:03:50 -0700

    Created rough draft of survey html. Currently not hooked to any js.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,15 @@
 module.exports = function (app) {
     app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, '../index.html'));
+        res.sendFile(path.join(__dirname, '../public/home.html'));
+    });
+
+    app.get('/index.html', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/home.html'));
     });
 
     // API Routes really only work as 
     app.get('/people', function (req, res) {
-        // stuff
+        res.sendFile(path.join(__dirname, '../public/survey.html'))
     });
 
 }
