a1ee27f020106da22a03eacff7682a197e88e056
Dan Gold <tentativechaos@gmail.com>
2019-09-23 23:43:59 -0700

    Added connection details for heroku mySQL database.

--- server.js
+++ server.js
@@ -15,14 +15,18 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 // MySQL DB Connection Information (remember to change this with our specific credentials)
-var connection = mysql.createConnection({
+var connection
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "",
   database: "nmitydb"
 });
-
+}
 // Initiate MySQL Connection.
 connection.connect(function(err) {
   if (err) {
